92d8c876af0fdf1050ba47bf4a929154fc66706b
ARNAB <arnabpal2002@gmail.com>
2024-09-18 08:32:44 +0530

    Updated the homepage

--- frontend/app/(pages)/home/page.tsx
+++ frontend/app/(pages)/home/page.tsx
@@ -1,94 +1,95 @@
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import Appbar from "../../components/Appbar";
+// import Doughnut from "@/app/components/ui/doughnut";
+// import { useSession } from "next-auth/react";
+// import { getUserInfo } from "@/app/api/utility/api";
+// import { UserData } from "@/interface/userInterface";
+// import { useRouter } from "next/navigation";
+
+// const Home: React.FC = () => {
+//   const { data: session, status } = useSession();
+//   const router = useRouter();
+//   const [userData, setUserData] = useState<UserData>({});
+//   const [loading, setLoading] = useState<boolean>(true);
+//   const [error, setError] = useState<string | null>(null);
+
+//   const user_id = session?.user?.user_id;
+
+//   // Fetch user data
+//   const fetchData = async (id: number) => {
+//     try {
+//       const data = await getUserInfo(`userInfo/${id}`);
+//       setUserData(data);
+//     } catch (err) {
+//       console.error("Error fetching user info:", err);
+//       setError("Failed to fetch user data.");
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   // Fetch data when the user is available
+//   useEffect(() => {
+//     if (user_id) {
+//       fetchData(user_id);
+//     }
+//   }, [user_id]);
+
+
+//   // By using useEffect, the component can render initially, and then if the status is unauthenticated, it triggers the redirection. This prevents a blocking operation that could slow down or even prevent the page from loading.
+//   useEffect(() => {
+//     if (status === "unauthenticated") {
+//       router.push("/signin");
+//     }
+//   }, [status, router]);
+
+//   if (status === "loading") {
+//     return <p>Loading...</p>;
+//   }
+
+//   // Destructure user financial info for easier access
+//   const {
+//     userFinancialInfo: {
+//       expenseAnalysis = {},
+//       debtManagement = {},
+//       investmentAdvice = {},
+//       goalRoadmap = {},
+//       growth = {},
+//       savingPlan = {},
+//       structuredPlan = {},
+//       summary = "",
+//     } = {},
+//   } = userData;
+
+//   return (
+//     <div>
+//       <Appbar />
+//       <div className="mx-4 my-6 border-2 border-normal-500 border-solid rounded-xl h-screen">
+//         <div className="flex justify-center items-center">
+//           {loading ? (
+//             <p>Generating the report...</p>
+//           ) : loading && error ? (
+//             <p className="text-red-500">{error}</p>
+//           ) : (
+//             <Doughnut expense={expenseAnalysis} />
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
+
+
+
+
 "use client";
-import React, { useEffect, useState } from "react";
-import Appbar from "../../components/Appbar";
-import Doughnut from "@/app/components/ui/doughnut";
-import { useSession } from "next-auth/react";
-import { getUserInfo } from "@/app/api/utility/api";
-import { UserData } from "@/interface/userInterface";
-import { useRouter } from "next/navigation";
-
-const Home: React.FC = () => {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-  const [userData, setUserData] = useState<UserData>({});
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  const user_id = session?.user?.user_id;
-
-  // Fetch user data
-  const fetchData = async (id: number) => {
-    try {
-      const data = await getUserInfo(`userInfo/${id}`);
-      setUserData(data);
-    } catch (err) {
-      console.error("Error fetching user info:", err);
-      setError("Failed to fetch user data.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Fetch data when the user is available
-  useEffect(() => {
-    if (user_id) {
-      fetchData(user_id);
-    }
-  }, [user_id]);
-
-
-  // By using useEffect, the component can render initially, and then if the status is unauthenticated, it triggers the redirection. This prevents a blocking operation that could slow down or even prevent the page from loading.
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      router.push("/signin");
-    }
-  }, [status, router]);
-
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
-
-  // Destructure user financial info for easier access
-  const {
-    userFinancialInfo: {
-      expenseAnalysis = {},
-      debtManagement = {},
-      investmentAdvice = {},
-      goalRoadmap = {},
-      growth = {},
-      savingPlan = {},
-      structuredPlan = {},
-      summary = "",
-    } = {},
-  } = userData;
-
-  return (
-    <div>
-      <Appbar />
-      <div className="mx-4 my-6 border-2 border-normal-500 border-solid rounded-xl h-screen">
-        <div className="flex justify-center items-center">
-          {loading ? (
-            <p>Generating the report...</p>
-          ) : loading && error ? (
-            <p className="text-red-500">{error}</p>
-          ) : (
-            <Doughnut expense={expenseAnalysis} />
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
-
-
-
-
-/*
-
-"use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { signOut, useSession } from "next-auth/react";
 import { Sidebar, SidebarBody, SidebarLink } from "../../components/ui/sidebar";
 import {
   IconArrowLeft,
@@ -98,10 +99,17 @@ import {
 } from "@tabler/icons-react";
 import Link from "next/link";
 import { motion } from "framer-motion";
-// import Image from "next/image";
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 
 export default function Home() {
+  const { data: session, status } = useSession();
+
+  const name = session?.user.name?.split(" ") || [];
+  const firstNameInitial = name.length > 0 ? name[0][0] : "";
+  const lastNameInitial = name.length > 1 ? name[1][0] : "";
+  const initials = `${firstNameInitial}${lastNameInitial}`
+
   const links = [
     {
       label: "Dashboard",
@@ -130,6 +138,8 @@ export default function Home() {
       icon: (
         <IconArrowLeft className="text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
+      onClick: () =>
+        console.log("Cloented")
     },
   ];
   const [open, setOpen] = useState(false);
@@ -153,21 +163,33 @@ export default function Home() {
             </div>
           </div>
           <div>
-            <SidebarLink
-              link={{
-                label: "Arnab",
-                href: "#",
-                // icon: (
-                //   // <Image
-                //   //   src="https://assets.aceternity.com/manu.png"
-                //   //   className="h-7 w-7 flex-shrink-0 rounded-full"
-                //   //   width={50}
-                //   //   height={50}
-                //   //   alt="Avatar"
-                //   // />
-                // ),
-              }}
-            />
+            {session?.user.image ? (
+              <SidebarLink
+                link={{
+                  label: session.user.name!,
+                  href: "#",
+                  icon: (
+                    <img
+                      src={session?.user.image!}
+                      className="h-7 w-7 flex-shrink-0 rounded-full"
+                      width={50}
+                      height={50}
+                      alt="Avatar"
+                    />
+                  ),
+                }}
+              />
+            ) : (
+              <div className="">
+                <div
+                  className="h-7 w-7 flex items-center justify-center bg-gray-500 text-white text-sm font-semibold rounded-full"
+                  style={{ width: 30, height: 30 }}
+                >
+                  {initials}
+                </div>
+                {session?.user.name}
+              </div>
+            )}
           </div>
         </SidebarBody>
       </Sidebar>
@@ -209,7 +231,7 @@ const Dashboard = () => {
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-700 bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex gap-2">
-          {[...new Array(4)].map((i) => (
+          {[...new Array(4)].map((_, i) => (
             <div
               key={"first" + i}
               className="h-20 w-full rounded-lg  bg-neutral-800 animate-pulse"
@@ -217,7 +239,7 @@ const Dashboard = () => {
           ))}
         </div>
         <div className="flex gap-2 flex-1">
-          {[...new Array(2)].map((i) => (
+          {[...new Array(2)].map((_, i) => (
             <div
               key={"second" + i}
               className="h-full w-full rounded-lg bg-neutral-800 animate-pulse"
@@ -228,9 +250,3 @@ const Dashboard = () => {
     </div>
   );
 };
-
-
-
-
-
-*/
